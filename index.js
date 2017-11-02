7a7128741e05535a0e5c00d3e7f7b25ab584bf6a
Natasha Oxley <natashareneeoxley@gmail.com>
2017-11-02 17:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var name = "Joe"
 
-if (name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected the name to be Susan")
-}
 var height = "74"
-
+var name = 'Doug'
+if (name === 'Susan') {
+  console.log('The name is right')
+} else {
+  console.log("This shouldn't print out")
+}
 
 // Don't worry about this
 module.exports = { name, height
